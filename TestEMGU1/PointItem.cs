116a41eb5f5cf53930e4e48d787e949933d5fbd4
apetrenko <apetrenko@gseis.ru>
2020-04-17 22:30:43 +0500

    renamed some classes and fields;
    made refactoring some classes;
    delete not used classes;

---
+++ TestEMGU1/PointItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+
+namespace TestEMGU1
+{
+    public readonly struct PointItem : IEquatable<PointItem>
+    {
+        public int Id { get; }
+
+        public PointF Point { get; }
+
+        public float Radius { get; }
+
+        public PointItem(int id, PointF point, float radius)
+        {
+            Id = id;
+            Point = point;
+            Radius = radius;
+        }
+
+        public bool Equals(PointItem other)
+        {
+            return Id == other.Id && Point == other.Point && Math.Abs(Radius - other.Radius) < 10e-5;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            return Equals((PointItem) obj);
+        }
+
+        public override int GetHashCode() => Id;
+    }
+}
