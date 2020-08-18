4e3d54dfae4367935d863298a2c5779c75f663fd
Dmitry Sherbakov <dvsherbakov@gmail.com>
2020-08-18 17:34:39 +0500

    add new projects

---
+++ PrepareImageFrm/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace PrepareImageFrm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.detectShapesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.savePreparedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.processToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveLogToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(5, 55);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(350, 350);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(361, 55);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(350, 350);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.processToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(719, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.detectShapesToolStripMenuItem,
+            this.savePreparedToolStripMenuItem,
+            this.saveLogToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(5, 412);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(706, 134);
+            this.listBox1.TabIndex = 3;
+            // 
+            // detectShapesToolStripMenuItem
+            // 
+            this.detectShapesToolStripMenuItem.Name = "detectShapesToolStripMenuItem";
+            this.detectShapesToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.D)));
+            this.detectShapesToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.detectShapesToolStripMenuItem.Text = "Detect Shapes";
+            this.detectShapesToolStripMenuItem.Click += new System.EventHandler(this.detectShapesToolStripMenuItem_Click);
+            // 
+            // savePreparedToolStripMenuItem
+            // 
+            this.savePreparedToolStripMenuItem.Name = "savePreparedToolStripMenuItem";
+            this.savePreparedToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.savePreparedToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.savePreparedToolStripMenuItem.Text = "Save Prepared";
+            this.savePreparedToolStripMenuItem.Click += new System.EventHandler(this.savePreparedToolStripMenuItem_Click);
+            // 
+            // processToolStripMenuItem
+            // 
+            this.processToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dirToolStripMenuItem});
+            this.processToolStripMenuItem.Name = "processToolStripMenuItem";
+            this.processToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.processToolStripMenuItem.Text = "Process";
+            // 
+            // dirToolStripMenuItem
+            // 
+            this.dirToolStripMenuItem.Name = "dirToolStripMenuItem";
+            this.dirToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.A)));
+            this.dirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.dirToolStripMenuItem.Text = "Dir...";
+            this.dirToolStripMenuItem.Click += new System.EventHandler(this.dirToolStripMenuItem_Click);
+            // 
+            // saveLogToolStripMenuItem
+            // 
+            this.saveLogToolStripMenuItem.Name = "saveLogToolStripMenuItem";
+            this.saveLogToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.L)));
+            this.saveLogToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.saveLogToolStripMenuItem.Text = "Save Log";
+            this.saveLogToolStripMenuItem.Click += new System.EventHandler(this.saveLogToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 556);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ToolStripMenuItem detectShapesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem savePreparedToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem processToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveLogToolStripMenuItem;
+    }
+}
+
---
+++ PrepareImageFrm/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+namespace PrepareImageFrm
+{
+    public partial class Form1 : Form
+    {
+        Image<Bgr, byte> imgInput;
+        string currentFile;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog
+                {
+                    RestoreDirectory = true
+                };
+                if (dialog.ShowDialog()== DialogResult.OK)
+                {
+                    imgInput = new Image<Bgr, byte>(dialog.FileName);
+                    pictureBox1.Image = imgInput.Bitmap;
+                    currentFile = dialog.FileName;
+                }
+            }
+            catch (Exception ex)
+            {
+                listBox1.Items.Insert(0, ex.Message);
+            }
+        }
+
+        private void detectShapesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (imgInput==null)
+            {
+                return;
+            } else
+            {
+                try
+                {
+                    var temp = imgInput.SmoothGaussian(15).Convert<Gray, byte>().ThresholdBinaryInv(new Gray(80),new Gray(255));
+                    VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+                    Mat m = new Mat();
+                    CvInvoke.FindContours(image: temp, contours, m, Emgu.CV.CvEnum.RetrType.External, Emgu.CV.CvEnum.ChainApproxMethod.LinkRuns);
+                    for (int i = 0; i < contours.Size; i++)
+                    {
+                        double perimeter = CvInvoke.ArcLength(contours[i], true);
+                        VectorOfPoint approx = new VectorOfPoint();
+                        CvInvoke.ApproxPolyDP(contours[i], approx, 0.03 * perimeter, true);
+                        CvInvoke.DrawContours(imgInput, contours, i, new MCvScalar(0, 0, 255));
+                        var rct = CvInvoke.FitEllipse(contours[i]);
+                        var aspct = getAspectRatio(rct);
+                        if (aspct < 0.33f)
+                            listBox1.Items.Insert(0, $"X: {rct.Center.X}, Y: {rct.Center.Y}, Width: {rct.Size.Width}, Height: {rct.Size.Height}, Aspect: {getAspectRatio(rct)}");
+                    }
+                    if (contours.Size == 2)
+                    {
+                        listBox1.Items.Insert(0, getDistanceBeforeCntr(contours[0], contours[1]));
+                    }
+                    else
+                    {
+                        listBox1.Items.Insert(0, $"{currentFile}: Contours count not is two");
+                    }
+
+                    pictureBox2.Image = imgInput.Bitmap;
+                    //pictureBox2.Image = temp.Bitmap;
+                }
+                catch (Exception ex)
+                {
+                    listBox1.Items.Insert(0, ex.Message);
+                }
+            }
+        }
+        private float getAspectRatio(RotatedRect rct)
+        {
+            try
+            {
+                return 1 - (rct.Size.Width / rct.Size.Height);
+            }
+            catch (Exception ex)
+            {
+                listBox1.Items.Insert(0, ex.Message);
+                return 1.0f;
+            }
+        }
+
+        private float getDistanceBeforeCntr(VectorOfPoint a, VectorOfPoint b)
+        {
+            var rctA = CvInvoke.FitEllipse(a);
+            var rctB = CvInvoke.FitEllipse(b);
+            return getDistance(rctA.Center, rctB.Center);
+        }
+
+        private float getDistance(PointF a, PointF b)
+        {
+
+            return (float)Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2));
+
+        }
+
+        private void savePreparedToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image.Save(@"ouput.jpeg", ImageFormat.Jpeg);
+        }
+
+        private string PrepareFile(string filename)
+        {
+            string res;
+            try
+            {
+                imgInput = new Image<Bgr, byte>(filename);
+                pictureBox1.Image = imgInput.Bitmap;
+                currentFile = filename;
+
+                var temp = imgInput.SmoothGaussian(15).Convert<Gray, byte>().ThresholdBinaryInv(new Gray(80), new Gray(255));
+                VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+                Mat m = new Mat();
+                CvInvoke.FindContours(image: temp, contours, m, Emgu.CV.CvEnum.RetrType.External, Emgu.CV.CvEnum.ChainApproxMethod.LinkRuns);
+                for (int i = 0; i < contours.Size; i++)
+                {
+                    double perimeter = CvInvoke.ArcLength(contours[i], true);
+                    VectorOfPoint approx = new VectorOfPoint();
+                    CvInvoke.ApproxPolyDP(contours[i], approx, 0.03 * perimeter, true);
+                    CvInvoke.DrawContours(imgInput, contours, i, new MCvScalar(0, 0, 255));
+                }
+
+                if (contours.Size == 2)
+                {
+                    var rctA = CvInvoke.FitEllipse(contours[0]);
+                    var rctB = CvInvoke.FitEllipse(contours[1]);
+                    var aspctA = getAspectRatio(rctA);
+                    var aspctB = getAspectRatio(rctB);
+                    if ((aspctA < 0.3f) && (aspctB < 0.3f)) {
+                        res =
+                            $"{filename}:{rctA.Center.X}:{rctA.Center.Y}:{rctA.Size.Width}:{rctA.Size.Height}" +
+                        $":{rctB.Center.X}:{rctB.Center.Y}:{rctB.Size.Width}:{rctB.Size.Height}" +
+                        $":{getDistanceBeforeCntr(contours[0], contours[1])}";
+                    }
+                    else
+                    {
+                        res = $"{currentFile}: Contours count not is two";
+                    }
+                }
+                else
+                {
+                    res = $"{currentFile}: Contours count not is two";
+                }
+            }
+            catch (Exception ex)
+            {
+                listBox1.Items.Add($"{filename}: {ex.Message}");
+                res = $"{filename}: {ex.Message}";
+            }
+
+            return res;
+        }
+
+        private void dirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FolderBrowserDialog dialog = new FolderBrowserDialog();
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    var files = Directory.GetFiles(dialog.SelectedPath);
+                    foreach (var file in files)
+                    {
+                        var tmpRes = PrepareFile(file);
+                        listBox1.Items.Add(tmpRes);
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                listBox1.Items.Insert(0, ex.Message);
+            }
+        }
+
+        private void saveLogToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            StreamWriter SaveFile = new System.IO.StreamWriter($"{DateTime.Now.ToShortDateString()}.csv");
+            foreach (var item in listBox1.Items)
+            {
+                SaveFile.WriteLine(item.ToString());
+            }
+        }
+    }
+}
+
+
+
+//let src = cv.imread('canvasInput');
+//let dst = cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
+//cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
+//cv.threshold(src, src, 100, 200, cv.THRESH_BINARY);
+//let contours = new cv.MatVector();
+//let hierarchy = new cv.Mat();
+//cv.findContours(src, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
+//let hull = new cv.Mat();
+//let defect = new cv.Mat();
+//let cnt = contours.get(0);
+//let lineColor = new cv.Scalar(255, 0, 0);
+//let circleColor = new cv.Scalar(255, 255, 255);
+//cv.convexHull(cnt, hull, false, false);
+//cv.convexityDefects(cnt, hull, defect);
+//for (let i = 0; i < defect.rows; ++i)
+//{
+//    let start = new cv.Point(cnt.data32S[defect.data32S[i * 4] * 2],
+//                             cnt.data32S[defect.data32S[i * 4] * 2 + 1]);
+//    let end = new cv.Point(cnt.data32S[defect.data32S[i * 4 + 1] * 2],
+//                           cnt.data32S[defect.data32S[i * 4 + 1] * 2 + 1]);
+//    let far = new cv.Point(cnt.data32S[defect.data32S[i * 4 + 2] * 2],
+//                           cnt.data32S[defect.data32S[i * 4 + 2] * 2 + 1]);
+//    cv.line(dst, start, end, lineColor, 2, cv.LINE_AA, 0);
+//    cv.circle(dst, far, 3, circleColor, -1);
+//}
+//cv.imshow('canvasOutput', dst);
+//src.delete(); dst.delete(); hierarchy.delete(); contours.delete(); hull.delete(); defect.delete();
