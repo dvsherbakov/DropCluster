ae20597a01191b24f98ad369381473df6f568f8c
Dmitry Sherbakov <dvsherbakov@gmail.com>
2021-02-09 18:10:33 +0500

    add Visor project

---
+++ TermoVisor/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace TermoVisor
+{
+   
+    public partial class MainWindow : Window
+    {
+        private PrepareCsv CsvData { get; }
+        private int f_PosX;
+        private int f_PosY;
+        private int f_ToolMode;
+        private Point f_PointOne;
+        private Point f_PointTwo;
+
+        public MainWindow()
+        {
+            CsvData = new PrepareCsv();
+            InitializeComponent();
+            f_ToolMode = 1;
+        }
+
+        private void OpenCsvFile(object sender, RoutedEventArgs e)
+        {
+            var dlg = new Microsoft.Win32.OpenFileDialog
+            {
+                DefaultExt = ".csv", Filter = "csv documents (.csv)|*.csv", RestoreDirectory = true
+            };
+            
+            var result = dlg.ShowDialog();
+            if (result != true) return;
+            CsvData.OpenFile(dlg.FileName);
+            SrcImage.Source = CsvData.GetImage;
+        }
+
+        private void OnImageMouseMove(object sender, MouseEventArgs e)
+        {
+            var position = Mouse.GetPosition(ImgContainer);
+            f_PosX = (int) position.X;
+            f_PosY = (int) position.Y;
+            var temp = CsvData.GetTemp(f_PosX, f_PosY);
+            Temperature.Text = temp.ToString("F1");
+        }
+
+        private void OnImageMouseDown(object sender, MouseEventArgs e)
+        {
+            switch (f_ToolMode)
+            {
+                case 1:
+                    f_PointOne = new Point(f_PosX, f_PosY);
+                    f_ToolMode = 2;
+                    break;
+                case 2:
+                    f_PointTwo = new Point(f_PosX, f_PosY);
+                    f_ToolMode = 3;
+                    break;
+                default:
+                    f_ToolMode = 1;
+                    break;
+            }
+        }
+
+        private void SetPointTool(object sender, RoutedEventArgs e)
+        {
+           f_ToolMode = 1;
+        }
+    }
+}
---
+++ TermoVisor/PrepareCsv.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace TermoVisor
+{
+    public class PrepareCsv
+    {
+        private string f_FileName;
+        private int Width { get; set; }
+        private int Height { get; set; }
+        private float f_Min;
+        private float f_Max;
+        private float f_K;
+        private BitmapImage f_Img;
+        public BitmapImage Img => f_Img;
+        private float[][] f_RawData;
+        public PrepareCsv()
+        {
+            Width = 0;
+            Height = 0;
+        }
+        public void OpenFile(string fName)
+        {
+            f_FileName = fName;
+            var result = new List<float[]>();
+
+            using (var reader = new StreamReader(fName))
+            {
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+
+                    if (line == null) continue;
+                    var values = line.Split(',').Select(a =>
+                    {
+                        if (float.TryParse(a, System.Globalization.NumberStyles.Any, CultureInfo.InvariantCulture, out var number))
+                            return number;
+                        return 0;
+                    }).ToArray();
+                    if (values.Length > Width) Width = values.Length;
+                    result.Add(values);
+                }
+            }
+            Height = result.Count;
+
+            GenerateBitmap(result.ToArray());
+        }
+
+        private void GenerateBitmap(float[][] data)
+        {
+            TwoDimMinMax(data);
+            var newBitmap = new Bitmap(Width, Height);
+
+
+            for (var j = 0; j < Height; j++)
+            {
+                for (var i = 0; i < Width; i++)
+                {
+                    var newColor = GetColorFromValue(data[j][i]);
+
+                    newBitmap.SetPixel(i, j, newColor);
+                }
+            }
+
+            f_RawData = data;
+            f_Img = BitmapToImageSource(newBitmap);
+        }
+
+        public BitmapImage GetImage => f_Img;
+
+        private System.Drawing.Color GetColorFromValue(float value)
+        {
+            var r = (int)((255 * (value - f_Min) * f_K) / 100);
+            var b = (int)((255 * (100 - (value - f_Min) * f_K)) / 100);
+
+            return System.Drawing.Color.FromArgb(r, 0, b);
+        }
+
+        private void TwoDimMinMax(float[][] data)
+        {
+            f_Max = data[0][0];
+            f_Min = data[0][0];
+
+            for (var j = 0; j < Width; j++)
+            {
+                for (var i = 0; i < Height; i++)
+                {
+                    if (data[i][j] > f_Max) f_Max = data[i][j];
+                    if (data[i][j] < f_Min) f_Min = data[i][j];
+                }
+            }
+
+            var zLvl = f_Max - f_Min;
+            f_K = 100 / zLvl;
+        }
+
+        private static BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using (var memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                var imageSource = new BitmapImage();
+                imageSource.BeginInit();
+                imageSource.StreamSource = memory;
+                imageSource.CacheOption = BitmapCacheOption.OnLoad;
+                imageSource.EndInit();
+
+                return imageSource;
+            }
+        }
+
+        public float GetTemp(int x, int y)
+        {
+            if (y > 0 && y < Height && x>0 && x<Width)
+                return f_RawData[y][x];
+            return 0f;
+        }
+    }
+}
