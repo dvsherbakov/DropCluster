d6cbc6457768d1dc816b99dac26386b229922035
Dmitry Sherbakov <dvsherbakov@gmail.com>
2021-02-14 20:34:26 +0500

    add come class

---
+++ HexagonalWpf/Cluster.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Emgu.CV.Structure;
+
+namespace HexagonalWpf
+{
+    class Cluster
+    {
+        private readonly List<ClusterElement> f_Cluster;
+        public string ClusterId { get; }
+
+        public int Count => f_Cluster.Count;
+        public Cluster(string fName)
+        {
+            f_Cluster = new List<ClusterElement>();
+            ClusterId = fName;
+        }
+
+        public void Add(ClusterElement el)
+        {
+            f_Cluster.Add(el);
+        }
+
+        public IEnumerable<ClusterElement> GetList => f_Cluster;
+
+        public int GetNearerId(RotatedRect el)
+        {
+            if (f_Cluster == null || f_Cluster.Count <= 0) return -1;
+            var tmp = f_Cluster.OrderBy(x => x.Range(el));
+            return f_Cluster.OrderBy(x => x.Range(el)).FirstOrDefault().Id;
+        }
+    }
+}
---
+++ HexagonalWpf/RawCluster.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexagonalWpf
+{
+    class RawCluster
+    {
+        private Cluster f_Cluster;
+        private string f_FileName;
+        public string FileName => f_FileName;
+
+        public RawCluster(string fName)
+        {
+            f_FileName = fName;
+        }
+    }
+}
