c5b7c43934fe0cfa42136b6e95800adadd2baec5
Dmitry Sherbakov <dvsherbakov@gmail.com>
2021-07-01 11:22:39 +0500

    first start app

---
+++ DeterminationOfBrightness/MainWindowModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+using Microsoft.Win32;
+
+
+namespace DeterminationOfBrightness
+{
+    internal class MainWindowModel : ViewModel
+    {
+        public ICommand QuitCommand { get; }
+
+        public MainWindowModel()
+        {
+            QuitCommand = new LambdaCommand(p => Application.Current.Shutdown());
+        }
+    }
+}
